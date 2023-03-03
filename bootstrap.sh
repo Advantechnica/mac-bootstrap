ac86ce520790c6819ee7219a2227ccfb6bf9a2fe
Aaron <Advantechnica@users.noreply.github.com>
2023-03-02 23:02:16 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Check if Homebrew is installed and install it if it's not
 if ! command -v brew &> /dev/null
 then
     read -p "Homebrew is not installed. Do you want to install Homebrew? (y/n) " choice
@@ -14,3 +15,25 @@ then
 else
     echo "Homebrew is already installed"
 fi
+
+# Define required packages
+packages=(iterm2 discord firefox google-chrome visual-studio-code slack vim nano curl python go terraform kubectl)
+
+# Loop through packages and check if they're installed, install them if they're missing
+for package in "${packages[@]}"
+do
+    if brew list "$package" &> /dev/null
+    then
+        echo "$package is already installed"
+    else
+        read -p "$package is not installed. Do you want to install $package? (y/n) " choice
+        case "$choice" in
+            y|Y )
+                echo "Installing $package..."
+                brew install "$package"
+                ;;
+            n|N ) echo "Skipping $package installation";;
+            * ) echo "Invalid choice. Skipping $package installation";;
+        esac
+    fi
+done
